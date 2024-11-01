8745b9efbc5d4fd7dc23a38fd20f2b580aea5a29
Airat Karimov <frigesty@gmail.com>
2024-11-01 16:29:44 +0300

    first commit

---
+++ src/test/java/GithubTests.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GithubTests {
+    @Test
+    void searchCodeJUnit5() {
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $("input#wiki-pages-filter").setValue("SoftAssertions");
+        $(".filterable-active").shouldHave(text("SoftAssertions"));
+        $(".filterable-active").$(byText("SoftAssertions")).click();
+        $$(".markdown-heading .heading-element").findBy(text("3. Using JUnit5 extend test class:"))
+                .closest(".markdown-heading")
+                .sibling(0)
+                .shouldHave(text(
+                        "@ExtendWith({SoftAssertsExtension.class})\n" +
+                        "class Tests {\n" +
+                        "  @Test\n" +
+                        "  void test() {\n" +
+                        "    Configuration.assertionMode = SOFT;\n" +
+                        "    open(\"page.html\");\n" +
+                        "\n" +
+                        "    $(\"#first\").should(visible).click();\n" +
+                        "    $(\"#second\").should(visible).click();\n" +
+                        "  }\n" +
+                        "}"));
+    }
+}
